001901ab2658a81dc89ca859924128c2cf77fe54
andry <andry.sunandar@gmail.com>
2016-04-07 22:22:50 +0700

    first commit

---
+++ src/main/java/com/bl/rekweb/ki/group232/Vehicle.java
@@ -0,0 +1,35 @@
+/**
+ * Innovez-One, Proprietary Software Cloud Communications
+ *  Copyright (c) 2015, Innovez-One and individual contributors
+ *  by the @authors tag.
+ *
+ *  This program is Proprietary Software: you can not redistribute it and/or modify
+ *  without license from Innovez-One.
+ *
+ *  Website : http://www.innovez-one.com/
+ *  Report bugs to <techsupport@innovez-one.com>.
+ *  Copyright (C) 2015 PT. Innovez-One. All rights reserved.
+ */
+package com.bl.rekweb.ki.group232;
+
+/**
+ * Author andry on 07/04/16.
+ */
+public class Vehicle {
+    
+    private Tire tire;
+    private String colour;
+    private boolean isAudio;
+    
+
+    public void forward(){
+
+        System.out.println("car in forward");
+
+    }
+
+    public void backward(){
+        System.out.println("car is backward");
+
+    }
+}
